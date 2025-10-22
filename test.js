4e765b8175aeb817abb258e63bd5e4f05b9d239d
filipkozlowskitestin01second <filip.kozlowski.test.in.01+second@gmail.com>
2025-10-22 11:03:34 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,23 @@
+function calculateArea(width, height) {
+  const area = width * height;
+  console.log(`Area is ${area}`);
+  return area;
+}
+
+function calculatePerimeter(width, height) {
+  const perimeter = 2 * (width + height);
+  console.log(`Perimeter is ${perimeter}`);
+  return perimeter;
+}
+
+function computeArea(width, height) {
+  const area = width * height;
+  console.log(`Area is ${area}`);
+  return area;
+}
+
+function computePerimeter(width, height) {
+  const perimeter = 2 * (width + height);
+  console.log(`Perimeter is ${perimeter}`);
+  return perimeter;
+}
