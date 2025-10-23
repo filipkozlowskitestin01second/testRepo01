a5db207af7edca7074c395ecf283f158ee537ea2
filipkozlowskitestin01second <filip.kozlowski.test.in.01+second@gmail.com>
2025-10-23 15:19:17 +0200

    Create logicT.js

---
+++ logicT.js
@@ -0,0 +1,18 @@
+function calculatePerimeter(width, height) {
+  const perimeter = 2 * (width + height);
+  console.log(`Perimeter is ${perimeter}`);
+  return perimeter;
+}
+
+function computeArea(width, height) {
+  const area = width * height;
+  console.log(`Area is ${area}`);
+  return area;
+}
+
+function computePerimeter(width, height) {
+  const perimeter = 2 * (width + height);
+  console.log(`Perimeter is ${perimeter}`);
+    const perimeter = 2 * (width + height);
+  return perimeter;
+}
